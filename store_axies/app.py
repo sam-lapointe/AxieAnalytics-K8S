af5db873577c04e699d6425ad755cbf3d567fc09
sam-lapointe <samuellapointebusiness@gmail.com>
2025-07-26 17:13:35 -0400

    Refactored store_axies to not use Azure services.

---
+++ store_axies/app.py
@@ -0,0 +1,107 @@
+import logging
+import asyncpg
+import asyncio
+import json
+from aio_pika import connect, Message
+from config import Config
+from parts import Part
+from axies import Axie
+
+
+# Global variables
+db_connection = None
+axie_api_key = None
+dependencies_lock = asyncio.Lock()
+dependencies_initialized = False
+
+
+async def init_dependencies():
+    """
+    Initialize dependencies for the function app.
+    This function is called when the function app starts.
+    """
+    global db_connection, axie_api_key, dependencies_initialized
+
+    if dependencies_initialized:
+        return
+
+    async with dependencies_lock:
+        if dependencies_initialized:
+            return
+
+        logging.info("Initializing dependencies...")
+
+        if not db_connection:
+            # Initialize PostgreSQL connection
+            db_connection_string = await Config.get_pg_connection_string()
+            db_connection = await asyncpg.create_pool(
+                dsn=db_connection_string,
+                min_size=1,
+                max_size=10,
+            )
+            logging.info("PostgreSQL connection initialized.")
+
+        if not axie_api_key:
+            axie_api_key = await Config.get_axie_api_key()
+            logging.info("Axie API key initialized.")
+
+        # Verify if the versions table contain the latest version of parts
+        await Part.search_and_update_parts_latest_version(db_connection)
+        current_parts_version = await Part.get_current_version(db_connection)
+        if not current_parts_version:
+            logging.error(
+                "Current parts version not found in the database. Please run the init_axie_parts function."
+            )
+            raise Exception(
+                "Current parts version not found in the database. Please run the init_axie_parts function."
+            )
+
+        dependencies_initialized = True
+
+
+async def process_message(message: Message):
+    """
+    Process a message from the RabbitMQ queue.
+    This function is called when a new message is received.
+    """
+    global db_connection, axie_api_key
+    try:
+        message_body = json.loads(message.body.decode("utf-8"))
+
+        await Axie(
+            db_connection,
+            axie_api_key,
+            message_body["transaction_hash"],
+            message_body["axie_id"],
+            message_body["sale_date"],
+        ).process_axie_data()
+
+        logging.info(
+            f"Processed Axie data for transaction {message_body['transaction_hash']} and axie ID {message_body['axie_id']}."
+        )
+    except Exception as e:
+        logging.error(f"Error processing message: {e}")
+        raise e
+
+
+async def store_axies():
+    # Ensure dependencies are initialized
+    await init_dependencies()
+
+    try:
+        connection = await connect(Config.get_rabbitmq_connection_string())
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(
+                Config.get_rabbitmq_queue_axies_name(), durable=True
+            )
+
+            await queue.consume(process_message)
+            await asyncio.Future()
+    except Exception as e:
+        logging.error(f"Error in store_axies: {e}")
+        raise e
+
+
+if __name__ == "__main__":
+    asyncio.run(store_axies())
--- store_axies/config.py
+++ store_axies/config.py
@@ -1,68 +1,48 @@
 import logging
 import os
-from urllib.parse import quote_plus
-from azure.keyvault.secrets.aio import SecretClient
-from azure.identity.aio import DefaultAzureCredential
 
 
 class Config:
     @staticmethod
-    def get_key_vault_url() -> str:
-        key_vault_name = os.getenv("KEY_VAULT_NAME")
-        if not key_vault_name:
-            logging.critical("KEY_VAULT_NAME is not set.")
-            raise ValueError("KEY_VAULT_NAME environment variable is required.")
-        return f"https://{key_vault_name}.vault.azure.net"
+    def get_rabbitmq_connection_string() -> str:
+        rabbitmq_host = os.getenv("RABBITMQ_HOST")
+        rabbitmq_port = os.getenv("RABBITMQ_PORT")
+        rabbitmq_user = os.getenv("RABBITMQ_USER")
+        rabbitmq_password = os.getenv("RABBITMQ_PASSWORD")
+
+        if not all([rabbitmq_host, rabbitmq_port, rabbitmq_user, rabbitmq_password]):
+            logging.critical("RabbitMQ connection details are not set.")
+            raise ValueError("RabbitMQ connection details are required.")
+
+        return f"amqp://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_host}:{rabbitmq_port}/"
 
     @staticmethod
-    def get_servicebus_full_namespace() -> str:
-        full_namespace = os.getenv("ServiceBusConnection__fullyQualifiedNamespace")
-        if not full_namespace:
-            logging.critical(
-                "ServiceBusConnection__fullyQualifiedNamespace is not set."
-            )
+    def get_rabbitmq_queue_axies_name() -> str:
+        queue_name = os.getenv("RABBITMQ_QUEUE_AXIES_NAME")
+        if not queue_name:
+            logging.critical("RABBITMQ_QUEUE_AXIES_NAME is not set.")
             raise ValueError(
-                "ServiceBusConnection__fullyQualifiedNamespace environment variable is required."
+                "RABBITMQ_QUEUE_AXIES_NAME environment variable is required."
             )
-        return full_namespace
+        return queue_name
 
     @staticmethod
-    def get_servicebus_topic_axies_name() -> str:
-        topic_axies_name = os.getenv("SERVICEBUS_TOPIC_AXIES_NAME")
-        if not topic_axies_name:
-            logging.critical("SERVICEBUS_TOPIC_AXIES_NAME is not set.")
-            raise ValueError(
-                "SERVICEBUS_TOPIC_AXIES_NAME environment variable is required."
-            )
-        return topic_axies_name
-
-    @staticmethod
-    def get_servicebus_topic_axies_subscription_name() -> str:
-        subscription_name = os.getenv("SERVICEBUS_AXIES_SUBSCRIPTION_NAME")
-        if not subscription_name:
-            logging.critical("SERVICEBUS_AXIES_SUBSCRIPTION_NAME is not set.")
-            raise ValueError(
-                "SERVICEBUS_AXIES_SUBSCRIPTION_NAME environment variable is required."
-            )
-        return subscription_name
-
-    @staticmethod
-    async def get_pg_connection_string(credential: DefaultAzureCredential) -> str:
+    async def get_pg_connection_string() -> str:
         try:
             # Retrieve environment variables.
-            kv_pg_username = os.getenv("KV_PG_USERNAME")
-            kv_pg_password = os.getenv("KV_PG_PASSWORD")
+            pg_username = os.getenv("PG_USERNAME")
+            pg_password = os.getenv("PG_PASSWORD")
             pg_host = os.getenv("PG_HOST")
             pg_port = os.getenv("PG_PORT")
             pg_database = os.getenv("PG_DATABASE")
 
             # Validate required environment variables.
-            if not kv_pg_username:
-                logging.critical("KV_PG_USERNAME is not set.")
-                raise ValueError("KV_PG_USERNAME environment variable is required.")
-            if not kv_pg_password:
-                logging.critical("KV_PG_PASSWORD is not set.")
-                raise ValueError("KV_PG_PASSWORD environment variable is required.")
+            if not pg_username:
+                logging.critical("PG_USERNAME is not set.")
+                raise ValueError("PG_USERNAME environment variable is required.")
+            if not pg_password:
+                logging.critical("PG_PASSWORD is not set.")
+                raise ValueError("PG_PASSWORD environment variable is required.")
             if not pg_host:
                 logging.critical("PG_HOST is not set.")
                 raise ValueError("PG_HOST environment variable is required.")
@@ -73,15 +53,6 @@ class Config:
                 logging.critical("PG_DATABASE is not set.")
                 raise ValueError("PG_DATABASE environment variable is required.")
 
-            async with SecretClient(
-                Config.get_key_vault_url(), credential
-            ) as key_vault_client:
-                # Retrieves the PostgreSQL Credentials from Key Vault and URL encodes them.
-                pg_username_secret = await key_vault_client.get_secret(kv_pg_username)
-                pg_password_secret = await key_vault_client.get_secret(kv_pg_password)
-                pg_username = quote_plus(pg_username_secret.value)
-                pg_password = quote_plus(pg_password_secret.value)
-
             connection_string = f"postgres://{pg_username}:{pg_password}@{pg_host}:{pg_port}/{pg_database}"
             return connection_string
 
@@ -90,7 +61,7 @@ class Config:
             raise e
 
     @staticmethod
-    async def get_axie_api_key(credential: DefaultAzureCredential) -> str:
+    async def get_axie_api_key() -> str:
         try:
             # Retrieve environment variable
             axie_api_key = os.getenv("AXIE_API_KEY_NAME")
@@ -100,14 +71,7 @@ class Config:
                 logging.critical("AXIE_API_KEY is not set.")
                 raise ValueError("AXIE_API_KEY environment variable is required.")
 
-            async with SecretClient(
-                Config.get_key_vault_url(), credential
-            ) as key_vault_client:
-                # Retrieve the API key from Key Vault.
-                axie_api_key_secret = await key_vault_client.get_secret(axie_api_key)
-                axie_api_key_value = axie_api_key_secret.value
-
-            return axie_api_key_value
+            return axie_api_key
         except Exception as e:
             logging.error("Error retrieving the Axie API key.")
             raise e
